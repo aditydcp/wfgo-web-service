2ad12b6580dc54bc60ce6359021820f8e62255ef
Aditya Cahyo <aditydcp@gmail.com>
2023-11-13 17:56:38 +0700

    create data struct w/ placeholders

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// pond represents data about a pond
+type pond struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// farm represents data about a farm
+type farm struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	// Ponds []pond  `json:"ponds"`
+}
+
+// placeholder dummy data
+var ponds = []pond{
+	{ID: "1", Name: "A"},
+	{ID: "2", Name: "B"},
+	{ID: "3", Name: "C"},
+	{ID: "4", Name: "ABZ"},
+}
+
+var farms = []farm{
+	{ID: "1", Name: "Blue Train"},
+	{ID: "2", Name: "Jeru"},
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Run("localhost:8080")
+}
